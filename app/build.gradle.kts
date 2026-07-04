9fa6ebaa7d571787dc6b297151601c3443a8168a
yaredawoke787-wq <yaredawoke787@gmail.com>
2026-07-04 12:58:00 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
     }
   }
   compileOptions {
